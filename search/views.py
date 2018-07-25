342f0fbfd8189b73213456506dab0fad9256dad5
sanjitown <sanjitown@163.com>
2018-07-25 17:57:32 +0800

    auto deploy

--- search/views.py
+++ search/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from .engine import engine
 import json
 # Create your views here.
-app=engine()
+#app=engine()
 def index(request):
     context={'a':'a'}
-    obj=render(request,'search/index.html',context=context)
+    obj=render(request,'search/result.html',context=context)
     return obj
 
 def search(request):
     if 'q' in request.GET:
         keyword=request.GET['q']
-        msg=app.search(keyword=keyword)
+        #msg=app.search(keyword=keyword)
+        msg = 'You searched for '+keyword
     else:
         msg = 'You submitted an empty form.'
 
